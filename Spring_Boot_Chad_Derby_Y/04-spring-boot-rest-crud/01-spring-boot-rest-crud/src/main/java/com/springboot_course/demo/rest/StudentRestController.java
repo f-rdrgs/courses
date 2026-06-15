ca8c6abfeca47a3004c0ccb86cb6def0ceaea890
f-rdrgs <255919737+f-rdrgs@users.noreply.github.com>
2026-06-15 15:43:38 +0200

    stopped at start of spring rest api design

--- Spring_Boot_Chad_Derby_Y/04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/springboot_course/demo/rest/StudentRestController.java
+++ Spring_Boot_Chad_Derby_Y/04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/springboot_course/demo/rest/StudentRestController.java
@@ -3,9 +3,6 @@ package com.springboot_course.demo.rest;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -45,31 +42,5 @@ public class StudentRestController {
 
 
         return theStudents.get(studentId);
-    }
-
-    // This handler takes care of StudentNotFound exceptions
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc){
-
-        StudentErrorResponse error = new StudentErrorResponse();
-
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setMessage(exc.getMessage());
-        error.setTimestamp(System.currentTimeMillis());
-
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-    }
-
-    // Handles any kind of exception (such as passing string instead of an id)
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(Exception exc){
-
-        StudentErrorResponse error = new StudentErrorResponse();
-
-        error.setStatus(HttpStatus.BAD_REQUEST.value());
-        error.setMessage(exc.getMessage());
-        error.setTimestamp(System.currentTimeMillis());
-
-        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 }
--- Spring_Boot_Chad_Derby_Y/04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/springboot_course/demo/rest/StudentRestExceptionHandler.java
+++ Spring_Boot_Chad_Derby_Y/04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/springboot_course/demo/rest/StudentRestExceptionHandler.java
@@ -1,8 +1,36 @@
 package com.springboot_course.demo.rest;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class StudentRestExceptionHandler {
 
+    // This handler takes care of StudentNotFound exceptions
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc){
+
+        StudentErrorResponse error = new StudentErrorResponse();
+
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(exc.getMessage());
+        error.setTimestamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    // Handles any kind of exception (such as passing string instead of an id)
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(Exception exc){
+
+        StudentErrorResponse error = new StudentErrorResponse();
+
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(exc.getMessage());
+        error.setTimestamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
 }
