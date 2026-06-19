abb74fef85d04a5c98294103eb67a2a4f48b9f4b
f-rdrgs <255919737+f-rdrgs@users.noreply.github.com>
2026-06-19 15:40:28 +0200

    finished part 05 Spring Rest API security

--- Spring_Boot_Chad_Derby_Y/05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ Spring_Boot_Chad_Derby_Y/05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -1,28 +1,42 @@
 package com.luv2code.springboot.cruddemo.security;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
 
+    // @Bean
+    // public InMemoryUserDetailsManager userDetailsManager() {
+
+    //     UserDetails john = User.builder().username("john").password("{noop}test123").roles("EMPLOYEE").build();
+
+    //     UserDetails mary = User.builder().username("mary").password("{noop}test123").roles("EMPLOYEE", "MANAGER").build();
+
+    //     UserDetails susan = User.builder().username("susan").password("{noop}test123").roles("EMPLOYEE", "MANAGER", "ADMIN").build();
+
+    //     return new InMemoryUserDetailsManager(john, mary, susan);
+    // }
+
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
 
-        UserDetails john = User.builder().username("john").password("{noop}test123").roles("EMPLOYEE").build();
+        // Define query to retrieve a user by username
+        jdbcUserDetailsManager.setUsersByUsernameQuery("select user_id, pw, active from members where user_id=?"); // ? will match the ID we give it (parameterized query)
 
-        UserDetails mary = User.builder().username("mary").password("{noop}test123").roles("EMPLOYEE", "MANAGER").build();
+        // define query to retrieve the authorities/roles by username
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery("select user_id, role from roles where user_id=?");
 
-        UserDetails susan = User.builder().username("susan").password("{noop}test123").roles("EMPLOYEE", "MANAGER", "ADMIN").build();
-
-        return new InMemoryUserDetailsManager(john, mary, susan);
+        return jdbcUserDetailsManager; // That's it
     }
 
     @Bean
