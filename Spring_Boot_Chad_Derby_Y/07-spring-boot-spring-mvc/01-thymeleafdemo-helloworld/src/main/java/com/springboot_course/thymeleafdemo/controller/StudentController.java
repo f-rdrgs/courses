1a2a84b1934e569ca343425efb664c928b3f11b1
f-rdrgs <255919737+f-rdrgs@users.noreply.github.com>
2026-06-23 17:42:01 +0200

    Stopped at 022 spring MVC Form

--- Spring_Boot_Chad_Derby_Y/07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/springboot_course/thymeleafdemo/controller/StudentController.java
+++ Spring_Boot_Chad_Derby_Y/07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/springboot_course/thymeleafdemo/controller/StudentController.java
@@ -2,18 +2,31 @@ package com.springboot_course.thymeleafdemo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.springboot_course.thymeleafdemo.model.Student;
 
 @Controller
 public class StudentController {
     
+    @GetMapping("/showStudentForm")
     public String showForm(Model theModel){
         
         Student theStudent = new Student();
         
         theModel.addAttribute("student",theStudent);
-        
+
         return "student-form";
+    }
+
+    @PostMapping("/processStudentForm")
+    public String processForm(@ModelAttribute("student") Student theStudent){
+        // Spring already gathers and defines the Student object's fields since we already specified it in the HTML
+
+        System.out.println("theStudent:" + theStudent.getFirstName() + " " + theStudent.getLastName());
+        
+        return "student-confirmation";
     }
 }
