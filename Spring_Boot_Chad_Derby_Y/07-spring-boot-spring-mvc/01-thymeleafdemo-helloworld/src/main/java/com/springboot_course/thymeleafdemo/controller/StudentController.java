ed58c84ac5a408239016f19aaed62f9a392a887d
f-rdrgs <255919737+f-rdrgs@users.noreply.github.com>
2026-06-30 16:00:19 +0200

    starting data validation

--- Spring_Boot_Chad_Derby_Y/07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/springboot_course/thymeleafdemo/controller/StudentController.java
+++ Spring_Boot_Chad_Derby_Y/07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/springboot_course/thymeleafdemo/controller/StudentController.java
@@ -1,5 +1,8 @@
 package com.springboot_course.thymeleafdemo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +14,15 @@ import com.springboot_course.thymeleafdemo.model.Student;
 @Controller
 public class StudentController {
     
+    @Value("${countries}")
+    private List<String> countries;
+
+    @Value("${languages}")
+    private List<String> languages;
+
+    @Value("${os}")
+    private List<String> os;
+    
     @GetMapping("/showStudentForm")
     public String showForm(Model theModel){
         
@@ -18,6 +30,12 @@ public class StudentController {
         
         theModel.addAttribute("student",theStudent);
 
+        // add the list to the model
+        theModel.addAttribute("countries", countries);
+
+        theModel.addAttribute("languages", languages);
+
+        theModel.addAttribute("os", os);
         return "student-form";
     }
 
@@ -25,7 +43,7 @@ public class StudentController {
     public String processForm(@ModelAttribute("student") Student theStudent){
         // Spring already gathers and defines the Student object's fields since we already specified it in the HTML
 
-        System.out.println("theStudent:" + theStudent.getFirstName() + " " + theStudent.getLastName());
+        System.out.println("theStudent:" + theStudent.getFirstName() + " " + theStudent.getLastName() + " " + theStudent.getCountry());
         
         return "student-confirmation";
     }
---
+++ Spring_Boot_Chad_Derby_Y/07-spring-boot-spring-mvc/validationdemo/src/main/java/com/springboot_course/mvc/ValidationdemoApplication.java
@@ -0,0 +1,13 @@
+package com.springboot_course.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidationdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidationdemoApplication.class, args);
+	}
+
+}
